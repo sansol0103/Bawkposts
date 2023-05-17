affa2bea965a96d69ec5e7d6c70032e7a1e67ee8
sansol0103 <sansol_01@pm.me>
2023-05-17 18:27:49 +0200

    "Added" carousel to index page
    Added carousel to the index page. Still needs animations and movement.

--- js/index.js
+++ js/index.js
@@ -15,10 +15,13 @@ function createHTML(posts) {
     if (posts.categories[0] === 3) {
     const container = document.querySelector('.featured-post');
 
-    const postsContainer = document.createElement('a');
-    postsContainer.href = "blog-specific.html?id=" + posts.id;
+    const postsContainer = document.createElement('div');
     postsContainer.className = 'posts-container';
     postsContainer.id = posts.id;
+
+    const title = document.createElement('h2');
+    title.innerText = posts.title.rendered;
+    postsContainer.append(title);
 
     if (posts._embedded['wp:featuredmedia']) {
         const image = document.createElement('img');
@@ -32,17 +35,49 @@ function createHTML(posts) {
         postsContainer.append(noImage);
     }
 
-    const title = document.createElement('h2');
-    title.innerText = posts.title.rendered;
-    postsContainer.append(title);
+    const button = document.createElement('a');
+    button.innerText = 'Read more';
+    button.className = 'read-more';
+    button.href = "blog-specific.html?id=" + posts.id;
+    postsContainer.append(button);
 
     container.append(postsContainer);
     }
 }
 
+// Carousel
+
+function createCarousel(posts) {
+    const carousel = document.querySelector("[data-carousel]");
+
+    const carouselContainer = document.createElement('a');
+    carouselContainer.href = "blog-specific.html?id=" + posts.id;
+    carouselContainer.className = 'carousel-container';
+    carouselContainer.id = posts.id;
+
+    const title = document.createElement('h2');
+    title.innerText = posts.title.rendered;
+    carouselContainer.append(title);
+
+
+    if (posts._embedded['wp:featuredmedia']) {
+        const image = document.createElement('img');
+        image.src = posts._embedded['wp:featuredmedia'][0].source_url;
+        image.alt = posts._embedded['wp:featuredmedia'][0].alt_text;
+        carouselContainer.append(image);
+    } else {
+        const noImage = document.createElement('p');
+        noImage.innerText = 'No image available';
+        carouselContainer.append(noImage);
+    }
+
+    carousel.append(carouselContainer);
+}
+
 function handlePost(posts) {
     for (let i = 0; i < posts.length; i++) {
         createHTML(posts[i]);
+        createCarousel(posts[i]);
     }
 }
 
