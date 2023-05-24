18af6bd3aa27f88a251caa97d39e58a1aaab716d
sansol0103 <sansol_01@pm.me>
2023-05-24 17:25:03 +0200

    Better form validation and max-width on body

--- js/contact.js
+++ js/contact.js
@@ -7,33 +7,50 @@ const subject = document.querySelector('#subject');
 const subjectError = document.querySelector('#subjectError');
 const message = document.querySelector('#message');
 const messageError = document.querySelector('#messageError');
+const successMessage = document.querySelector('#successMessage');
 
 function validateForm(event) {
     event.preventDefault();
+    let formValidator = false;
 
-    if (checkLength(fullName.value, 1) === true) {
+    if (checkLength(fullName.value, 4) === true) {
         fullNameError.style.display = 'none';
     } else {
         fullNameError.style.display = 'block';
+        formValidator = true;
     }
     if (checkLength(subject.value, 14) === true) {
         subjectError.style.display = 'none';
     } else {
         subjectError.style.display = 'block';
+        formValidator = true;
     }
     if (checkLength(message.value, 24) === true) {
         messageError.style.display = 'none';
     } else {
         messageError.style.display = 'block';
+        formValidator = true;
     }
     if (validateEmail(email.value)) {
         emailError.style.display = 'none';
     } else {
         emailError.style.display = 'block';
+        formValidator = true;
+    }
+
+    if (formValidator) {
+        successMessage.style.display = 'none';
+    } else {
+        successMessage.style.display = 'block';
     }
 }
 
 form.addEventListener('submit', validateForm);
+
+form.onsubmit = function (event) {
+    event.preventDefault();
+    form.reset();
+};
 
 function checkLength(value, len) {
     if (value.trim().length > len) {
